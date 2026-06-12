25ef066278aed9e0a4aaa79b9fef28430e972dd0
Thomas Garrafa <garrafa435@gmail.com>
2026-06-12 18:34:10 +0200

    Moved "read_file" utility function

--- FastDownward/src/search/utils/asp_fact_converter.cc
+++ FastDownward/src/search/utils/asp_fact_converter.cc
@@ -1,6 +1,12 @@
 #include "asp_fact_converter.h"
 
+#include <fstream>
+#include <fstream>
+#include <sstream>
+#include <cctype>
+
 namespace utils{
+
     std::string atom_to_asp(const std::string &factname) {
         const std::string pref = "Atom ";
         if (factname.rfind(pref, 0) != 0)
@@ -10,11 +16,20 @@ namespace utils{
         out.reserve(body.size());
         for (char c : body)
             if (c != ' ')
-                out += c;
+                out += (c == '-') ? '_' : c;
         if (out.size() >= 2 && out.substr(out.size() - 2) == "()")
             out = out.substr(0, out.size() - 2);
         for (char &c : out)
             c = (char)::tolower((unsigned char)c);   // ASP: nomi minuscoli
         return out;
+    }
+
+    std::string read_file(const std::string &path) {
+        std::ifstream f(path);
+        if (!f)
+            throw std::runtime_error("[clingo] Unable to read: " + path);
+        std::stringstream ss;
+        ss << f.rdbuf();
+        return ss.str();
     }
 }
--- FastDownward/src/search/utils/asp_fact_converter.h
+++ FastDownward/src/search/utils/asp_fact_converter.h
@@ -2,10 +2,10 @@
 #define ASP_FACT_CONVERTER_H
 
 #include <string>
-#include <cctype>
 
 namespace utils{
     std::string atom_to_asp(const std::string &factname);
+    std::string read_file(const std::string &path);
 }
 
 #endif
